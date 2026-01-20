4fd6b8c6b20bd97390d57a146c2fa12ce97da76d
이선휘 <shine379@naver.com>
2026-01-20 14:42:55 +0900

    🔨 Chore: MR 자동화 삭제

--- scripts/2_setup_alias.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# git mr 명령어가 utils 폴더의 스크립트를 실행하도록 연결합니다.
-
-echo "🔨 Git MR Alias를 연결합니다..."
-
-# git alias 등록 (경로를 동적으로 찾아서 실행)
-git config --global alias.mr '!sh "$(git rev-parse --show-toplevel)/scripts/utils/generate_mr.sh"'
-
-echo "✅ Alias 연결 완료"
