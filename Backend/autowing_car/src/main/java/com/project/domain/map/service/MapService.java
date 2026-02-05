4864504a788356b613623378279a4de405db0577
jokong98 <spthplz@naver.com>
2026-02-05 14:21:11 +0900

    ✨ Feat: fix path bug

--- Backend/autowing_car/src/main/java/com/project/domain/map/service/MapService.java
+++ Backend/autowing_car/src/main/java/com/project/domain/map/service/MapService.java
@@ -295,7 +295,8 @@ public class MapService {
                 if (usageManager.isEdgeLocked(edge.getId()) || usageManager.isNodeLocked(neighbor.getId()))
                     continue;
 
-                double newG = gScore.getOrDefault(currentNode.getId(), Double.MAX_VALUE); // + edge.getTravelTime();
+                double newG = gScore.getOrDefault(currentNode.getId(), Double.MAX_VALUE)
+                        + (edge.getTravelTime() != null ? edge.getTravelTime() : edge.getDistance() / 10.0);
                 if (newG < gScore.getOrDefault(neighbor.getId(), Double.MAX_VALUE)) {
                     cameFrom.put(neighbor.getId(), edge);
                     gScore.put(neighbor.getId(), newG);
